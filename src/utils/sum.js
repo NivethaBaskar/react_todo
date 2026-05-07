5b7282973707046c25588ca29b71407434f0e67c
NivethaBaskar <44889675+NivethaBaskar@users.noreply.github.com>
2026-05-07 08:05:12 +0530

    feat(utils): add sum utility function

---
+++ src/utils/sum.js
@@ -0,0 +1,13 @@
+/**
+ * Sums all numbers in an array.
+ *
+ * @param {number[]} numbers - An array of numbers to sum.
+ * @returns {number} The sum of all numbers in the array. Returns 0 if the array is empty.
+ * @throws {TypeError} If the input is not an array.
+ */
+export default function sum(numbers) {
+    if (!Array.isArray(numbers)) {
+        throw new TypeError('Input must be an array');
+    }
+    return numbers.reduce((acc, num) => acc + num, 0);
+}
